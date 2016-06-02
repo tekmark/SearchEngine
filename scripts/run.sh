e93df5a68de500f387106074b5d0563c09a93c3f
Chao Han <hanchao.tju@gmail.com>
2016-06-02 01:27:47 -0400

    added a script directory.

---
+++ scripts/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+NUTCH_HOME="/Users/chaohan/apache-nutch-1.11";
+nutch="$NUTCH_HOME/bin/nutch"
+
+segments="$NUTCH_HOME/crawl/segments"
+crawldb="$NUTCH_HOME/crawl/crawldb"
+
+echo "Config Info:";
+echo $segments
+echo $crawldb
+#bash nutch
+
+
